e6a0ae73ae058546e825a989181352933ffd0fa3
usopyon <t.uehara@gmail.com>
2015-06-25 00:00:23 +0900

    URIでArduinoに送出するコマンドを指定し、ArduinoからのレスポンスをHTTPに出力

--- bottle_serial.py
+++ bottle_serial.py
@@ -1,19 +1,25 @@
+# -*- coding: utf-8 -*-
+
 import serial
 import sys
 from bottle import response,route,run
 
 try:
-    ser = serial.Serial('/dev/tty.usbmodem1411',9600)
+    ser = serial.Serial('/dev/tty.usbmodem1451',9600)
 except OSError:
     ser = False
 except:
     print sys.exc_info()[0]
+    raise
 
-@route('/arduino')
-def arduino():
-    if ser:
-        ser.write("1")
+@route('/arduino/<command>')
+def arduino(command):
     response.set_header('Access-Control-Allow-Origin','*')
-    return "Arduino!"
+    if ser:
+        ser.write(command)
+        line = ser.readline()
+        return line
+    else:
+        return "arduino is not connected"
 
 run(host="localhost",port=8946,debug=True)
