cc5e413b6c265bffaae2ca0cda815593bb00c61a
usopyon <t.uehara@gmail.com>
2015-11-01 21:54:03 +0900

    USBのポートを自動指定（Macのみ）

--- bottle_serial.py
+++ bottle_serial.py
@@ -3,9 +3,14 @@
 import serial
 import sys
 from bottle import response,route,run
+import os
 
 try:
-    ser = serial.Serial('/dev/tty.usbmodem1451',9600)
+    files = os.listdir('/dev')
+    for file in files:
+        if "tty.usbmodem" in file:
+            ser = serial.Serial('/dev/'+file,9600)
+            print file
 except OSError as (errno,strerror):
     if errno == 2 : # No such file or directory
         print "USB device is not connected"
@@ -19,11 +24,20 @@ except:
 @route('/arduino/<command>')
 def arduino(command):
     response.set_header('Access-Control-Allow-Origin','*')
-    if ser:
+    try:
         ser.write(command)
-        line = ser.readline()
-        return line
-    else:
-        return "arduino is not connected"
+        #line = ser.readline()
+        #return line
+    except:
+        print sys.exc_info()[0]
+    return "success"
 
-run(host="localhost",port=8946,debug=True)
+@route('/test')
+def arduino():
+    response.set_header('Access-Control-Allow-Origin','*')
+    return "test"
+
+if "usbmodem" in file:
+    run(host="127.0.0.1",port=8946,debug=True)
+else:
+    print "failed"
